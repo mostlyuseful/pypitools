a6060891b471401a5f79509844312961ecf7fb42
Maurice-Pascal Sonnemann <mpsonnemann@gmail.com>
2016-11-03 16:21:11 +0100

    Added inline update function
    Converted to click app, program gained following flags:
    --no-update : just list outdated packages
    --update    : try to update outdated packages
    Internally, pip is called as a library.

--- pypiupdate.py
+++ pypiupdate.py
@@ -2,11 +2,30 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
+
+import logging
+LOGGING_FORMAT = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+logging.basicConfig(format=LOGGING_FORMAT, level=logging.INFO)
+
 from pkgtools.pypi import PyPIJson
 import pip
 import progressbar
 import traceback
+import click
 from threading import Thread
+
+
+import pip.commands.install
+
+def install_distributions(distributions, args=None):
+    """From http://threebean.org/blog/2011/06/06/installing-from-pip-inside-python-or-a-simple-pip-api/
+    """
+    command = pip.commands.install.InstallCommand()
+    opts, args = command.parser.parse_args(args)
+    # TBD, why do we have to run the next part here twice before actual install
+    requirement_set = command.run(opts, distributions)
+    requirement_set = command.run(opts, distributions)
+    requirement_set.install(opts)
 
 try:
     # Python 2
@@ -26,11 +45,20 @@ def worker():
         except Exception as e:
             print("""Error occurred while checking package "{0}":""".format(pkg))
             print(traceback.format_exc())
+            try:
+                attrs = vars(e)
+                print('\n'.join(".{0} == {1}".format(key,val) for key,val in attrs.items()))
+            except: pass
         done.append(pkg)
         progress.update(len(done))
         q.task_done()
 
-if __name__=='__main__':
+
+def get_outdated():
+    global progress
+    global q
+    global done
+    global outdated
     
     outdated = []
     progress = None
@@ -52,10 +80,31 @@ if __name__=='__main__':
         q.put(pkg)
     q.join()
     progress.finish()
+    return outdated
+
+def update_packages(pkg_list):
+    for pkg in pkg_list:
+        try:
+            print("Updating {0}".format(pkg))
+            install_distributions([pkg], ['-U','-v'])
+            print("OK: Updated {0}".format(pkg))
+        except Exception as e:
+            print("""Error occurred while updating package "{0}":""".format(pkg))
+            print(traceback.format_exc())
+
+@click.command()
+@click.option('--update/--no-update', default=False, help='Update or list only')
+def main(update):
+    outdated = get_outdated()
 
     if outdated:
-        print("Run the following command to update all outdated packages:")
-        #print("pip install -U " + " ".join(["%s==%s"%(pkg.project_name,version) for (pkg,version) in outdated]))
-        print("pip install -U " + " ".join(sorted(pkg.project_name for (pkg,version) in outdated)))
+        if update:
+            update_packages(sorted(pkg.project_name for (pkg,version) in outdated))
+        else:
+            print("Run the following command to update all outdated packages:")
+            print("pip install -U " + " ".join(sorted(pkg.project_name for (pkg,version) in outdated)))
     else:
         print("Everything is up to date.")
+
+if __name__=='__main__':
+    main()
